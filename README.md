05c2d86887b789433b005f7a1f07b026696fbc7a
Elisha Kramer <kramer1346@gmail.com>
2020-05-08 17:15:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://shopify.dev/tutorials/build-a-shopify-app-with-node-and-react/fetch-data-with-apollo
---
+++ pages/_app.js
@@ -0,0 +1,48 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
+import '@shopify/polaris/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
+import Cookies from 'js-cookie';
+import { ApolloProvider } from 'react-apollo';
+
+import fetch from 'node-fetch'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import ApolloClient from 'apollo-client'
+
+const client = new ApolloClient({
+	  link: createHttpLink({
+  uri: 'https://api.domain.com/graphql',
+  fetch: fetch,
+  }),
+  cache: new InMemoryCache(),
+})
+
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+          <AppProvider>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+          </ApolloProvider>
+          </AppProvider>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
