2942be69080cc3e789202d5b5140f8951854e201
Elisha Kramer <kramer1346@gmail.com>
2020-05-12 13:51:10 +0300

    fixed cors bug for running app on development - still may cause errors on heroku server - and will need to add more logic to handle ApolloClient on Production server:

--- pages/_app.js
+++ pages/_app.js
@@ -2,49 +2,21 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
-import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-import ApolloClient from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloLink, concat } from 'apollo-link';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import 'cross-fetch/polyfill';
-
-// Create ApolloClient Instance and point to your Shopify store's GraphQl server.
-// const httpLink = new HttpLink({ uri: `https://${Cookies.get("shopOrigin")}.myshopify.com/admin/api/2020-04/graphql` });
-// const httpLink = new HttpLink({ uri: `https://shopify-graphiql-app.shopifycloud.com/admin/api/2020-04/graphql` });
-const httpLink = new HttpLink({ uri: `http://shopify-community-app.herokuapp.com/graphql` });
-
-
-
-// Add authentication headers
-const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-    operation.setContext({
-      headers: {
-        'X-Shopify-Storefront-Access-Token': `${Cookies.get("accessToken")}`
-      } 
-    });
-
-    return forward(operation);
-  })
-
-  const client = new ApolloClient({
-  link: concat(authMiddleware, httpLink),
-    cache: new InMemoryCache(),
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include',
+  },
 });
-
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    console.log('API_KEY: ',API_KEY)
     const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
 
     return (
