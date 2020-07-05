da2dfe5961385f130e119f591cf080f0419b08b5
Elisha Kramer <kramer1346@gmail.com>
2020-07-05 14:15:47 +0300

    loads manage posts view from more than one user, contains shopify billing feature, and Resourcelist with GraphQL

--- Admin-Frontend/server.js
+++ Admin-Frontend/server.js
@@ -58,31 +58,39 @@ app.prepare().then(() => {
           sameSite: 'none'
         });
 
-        shopifyScope = 'read_products, write_products, read_content, write_content' 
-        let new_shop = new Shop({ shopify_domain: shop, accessToken, shopifyScope})
-          new_shop.save((err, shopReturned) => {
-              if (err) {
-                  console.log('error: ', err)
-              } else {
-                  console.log('shop created and returned: ', shopReturned);
-              }
-          });
+        Shop.findOne({ shopify_domain: shop }).exec((err, shop) => {
+            if (err){
+                console.log('ran error logic. err:', err);          
+            } else if (!shop){
+                console.log('ran no shop found logic');
+                
+                shopifyScope = 'read_products, write_products, read_content, write_content'; 
+                let new_shop = new Shop({ shopify_domain: shop, accessToken, shopifyScope})
+                new_shop.save((err, shopReturned) => {
+                  if (err) {
+                    console.log('err trying to save shop: ', err)
+                  } else {
+                    console.log('shop successfully created');
+                    const registration = registerWebhook({
+                     address: `${HOST}/webhooks/products/create`,
+                     topic: 'PRODUCTS_CREATE',
+                     accessToken,
+                     shop,
+                     apiVersion: ApiVersion.October19
+                   });
 
-        const registration = await registerWebhook({
-         address: `${HOST}/webhooks/products/create`,
-         topic: 'PRODUCTS_CREATE',
-         accessToken,
-         shop,
-         apiVersion: ApiVersion.October19
-       });
+                   if (registration.success) {
+                     console.log('Successfully registered webhook!');
+                   } else {
+                     console.log('Failed to register webhook', registration.result);
+                   }
 
-       if (registration.success) {
-         console.log('Successfully registered webhook!');
-       } else {
-         console.log('Failed to register webhook', registration.result);
-       }
-
-        await getSubscriptionUrl(ctx, accessToken, shop);
+                    getSubscriptionUrl(ctx, accessToken, shop);
+                  }});
+            } else {
+                console.log('shop found: ', shop)   
+            }
+        });
       },
     }),
   );
