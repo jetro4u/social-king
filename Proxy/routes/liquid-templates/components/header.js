2b0f442f696207c4fb3dc1d512ed0c919e691cb0
Elisha Kramer <kramer1346@gmail.com>
2020-07-09 09:35:44 +0300

    added header component to the tagsSlug view

--- Proxy/routes/liquid-templates/components/header.js
+++ Proxy/routes/liquid-templates/components/header.js
@@ -1,10 +1,10 @@
-exports.header = ({shop}) => {
+exports.header = ({shop, tag}) => {
     return `
 		<header class="community-header">
             <div class="community-header-title">
                 <div class="page-width community-header-title-box">
                     <img src="https://styles.redditmedia.com/t5_2qiuc/styles/communityIcon_s6mblzvh8t051.png?width=256&s=851a80f05e1194ee216a999370fe45bdfd2b5010" class="community-icon" />
-                    <div class="community-h2">Home Improvement Community</div>
+                    <div class="community-h2">${tag ? "Posts Tagged '"+ tag.name+"'" : 'Home Improvement Community'}</div>
                 </div>
             </div>
         </header>
--- Proxy/routes/liquid-templates/tagSlug.js
+++ Proxy/routes/liquid-templates/tagSlug.js
@@ -1,43 +1,27 @@
 const { header } = require('./components/header');
 const { navbar } = require('./components/navbar');
+const { newsFeed } = require('./components/newsFeed');
 const { newsFeedCSS } = require('./css/newsFeedCSS');
+const proxyRoute = process.env.PROXY_ROUTE;
 
-exports.tagSlug = ({ shop, tags, tag, blogs, size}) => {
-    const proxyRoute = process.env.PROXY_ROUTE;
-
-    const showLoadedBlogs = () => {
-        return blogs.map((blog, i) => `
-                <div class="col-md-4 pt-3">
-                    <h3><a href="${proxyRoute}/blog/${blog.slug}">${blog.title}</a></h3>
-                    Image Goes Here
-                    <p>${blog.mdesc}</p>
-                </div>
-            `).join('');
-    };
-
+exports.tagSlug = ({ shop, blogs, tags, size, tag }) => {
+    
     return `
         <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
         <style type="text/css">
-            ${newsFeedCSS(shop)}
+            ${newsFeedCSS({shop})}
         </style>
-        ${header({shop})}
-        <main>
-            <div class="container-fluid">
-                <header>
-                    <div class="col-md-12 pt-3">
-                        <h1 class="display-4 font-weight-bold text-center">
-                            ${tag.name}
-                        </h1>
-                    </div>
-                    
-                </header>
-            </div>
-            <div class="row">${showLoadedBlogs()}</div>
-        </main>
-        ${navbar({shop, tags})}
+        ${header({shop, tag})}
+        <div class="community-background">
+            <main class="page-width">
+                <div class="pure-g">
+                    ${newsFeed({shop, blogs})}
+                    ${navbar({shop, tags})}
+                </div>
+            </main>
+        </div>
         <script>
             console.log('injected script from server ran');
-        </script>
-        
+        </script>    
         `
 };
