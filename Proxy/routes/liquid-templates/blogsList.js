86a59ff83b74c59a760e3071363c978ec2deb664
Elisha Kramer <kramer1346@gmail.com>
2020-07-09 08:32:56 +0300

    moved css into its own file, and imporiting navbar with the NewsFeedsCSS within the tagSlug view

--- Proxy/routes/liquid-templates/blogsList.js
+++ Proxy/routes/liquid-templates/blogsList.js
@@ -1,4 +1,5 @@
 const { navbar } = require('./navbar');
+const { newsFeedCSS } = require('./css/newsFeedCSS');
 
 exports.blogsList = ({ shop, blogs, categories, tags, size }) => {
     const proxyRoute = process.env.PROXY_ROUTE;
@@ -19,6 +20,9 @@ exports.blogsList = ({ shop, blogs, categories, tags, size }) => {
 
     return `
         <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
+        <style type="text/css">
+            ${newsFeedCSS(shop)}
+        </style>
         <header class="community-header">
             <div class="community-header-title">
                 <div class="page-width community-header-title-box">
@@ -50,35 +54,5 @@ exports.blogsList = ({ shop, blogs, categories, tags, size }) => {
         <script>
             console.log('injected script from server ran');
         </script>
-        <style type="text/css">
-            .community-background { background-color: #edeff1;padding-top:20px; }
-            .l-box { padding:1em; }
-            .tag-btn { margin-top: 1em;
-                      margin-bottom: 1em;
-                      margin-right: 1em;
-                      margin-left: 1em;
-                      background-color: lightblue; }
-            .tag-btn:hover {
-              background-color: lightgreen;
-            }
-            .community-icon { border:4px solid #fff;border-radius:100%;max-width:75px;margin-top:-10px; }
-            .community-header { background-image: url("https://previews.123rf.com/images/donatas1205/donatas12051202/donatas1205120200008/12455190-carpentry-construction-tools-home-improvement-background-.jpg");padding-top:120px;margin-top:-55px }
-            .community-header-title { background:#fff; }
-            .community-header-title-box { display:flex }
-            .community-header-title .community-h2 { display:inline-block;margin-left:20px;font-size:30px;font-weight:bold;color:#000;padding-top:10px; }
-            .community-card { border:1px solid #ccc;border-radius:4px;margin-bottom:20px;background:#fff }
-            .community-card-header { background:darkblue;color:white;padding:10px;font-weight:bold; }
-            .community-card-body { padding:10px }
-            .community-pad-20 { padding:10px 20px 20px 20px }
-            .community-hr { margin:20px 0 }
-            .community-hr-minimal { margin:8px 0 }
-            .community-full-width { width: 100% }
-            .community-instant-post { width:100%;border:1px solid #edeff1; background: #f6f7f8;padding:10px }
-            .community-instant-post:hover, .community-instant-post:focus { background:#fff }
-            .community-author { color:#969696;font-size:12px;margin-bottom:5px }
-            .community-card h3 { font-size:20px;margin-bottom:8px }
-            .community-card p { margin-bottom:8px }
-            .community-card-comments { font-size:12px;font-weight:bold; }
-        </style>
         `
 };
