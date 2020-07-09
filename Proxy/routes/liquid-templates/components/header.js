12a2a77b1e63006bd8ed4fa922b86cc280ae303d
Elisha Kramer <kramer1346@gmail.com>
2020-07-09 10:14:50 +0300

    added header to all proxy_app views

--- Proxy/routes/liquid-templates/components/header.js
+++ Proxy/routes/liquid-templates/components/header.js
@@ -1,10 +1,24 @@
-exports.header = ({shop, tag}) => {
+exports.header = ({shop, tag, user, blog}) => {
+	const showTitle = ()=>{
+		if(tag){
+    		return "Posts Tagged '"+ tag.name+"'"
+    	} else if (user) {
+    		return user.name
+    	} else if (blog) {
+    		return blog.title
+    	} else {
+    		return 'Home Improvement Community'
+    	}
+	}
+
+	const shopIcon = "https://styles.redditmedia.com/t5_2qiuc/styles/communityIcon_s6mblzvh8t051.png?width=256&s=851a80f05e1194ee216a999370fe45bdfd2b5010";
+
     return `
 		<header class="community-header">
             <div class="community-header-title">
                 <div class="page-width community-header-title-box">
-                    <img src="https://styles.redditmedia.com/t5_2qiuc/styles/communityIcon_s6mblzvh8t051.png?width=256&s=851a80f05e1194ee216a999370fe45bdfd2b5010" class="community-icon" />
-                    <div class="community-h2">${tag ? "Posts Tagged '"+ tag.name+"'" : 'Home Improvement Community'}</div>
+                    <img src="${user && user.cover_photo ? user.cover_photo : shopIcon}" class="community-icon" />
+                    <div class="community-h2">${showTitle()}</div>
                 </div>
             </div>
         </header>
