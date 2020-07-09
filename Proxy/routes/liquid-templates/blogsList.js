de4a62ebf69837d96558769c4832a25e200dc068
Elisha Kramer <kramer1346@gmail.com>
2020-07-09 07:59:47 +0300

    moved the navbar into its own template function, and imported it into blogslist - page loads correctly

--- Proxy/routes/liquid-templates/blogsList.js
+++ Proxy/routes/liquid-templates/blogsList.js
@@ -1,3 +1,5 @@
+const { navbar } = require('./navbar');
+
 exports.blogsList = ({ shop, blogs, categories, tags, size }) => {
     const proxyRoute = process.env.PROXY_ROUTE;
     
@@ -13,14 +15,6 @@ exports.blogsList = ({ shop, blogs, categories, tags, size }) => {
                      </div>
                 </div>
             `).join('');
-    };
-
-     const showAllTags = () => {
-        return tags.map((t, i) => `
-            <a href="${proxyRoute}/tags/${t.slug}" key=${i}>
-                <button class="tag-btn pure-button pure-button-primary">${t.name}</a>
-            </a>
-        `).join('');
     };
 
     return `
@@ -49,35 +43,7 @@ exports.blogsList = ({ shop, blogs, categories, tags, size }) => {
                             <div class="pure-g">${showLoadedBlogs()}</div>
                         </div>
                     </div>
-                    <div class="pure-u-1-3">
-                        <div class="community-pad-20">
-                            <div class="community-card">
-                                <div class="community-card-header">About Community</div>
-                                <div class="community-card-body">Welcome to the home improvement community page! Share your creations with fellow builders, and get inspired for new projects.
-                                <hr class="community-hr" />
-                                <a href='https://${shop.shopify_domain+proxyRoute}/user/profile?email={{ customer.email }}&name={{ customer.name }}&hash={{ customer.email | append: "somecrazyhash" | md5 }}'><button class="pure-button pure-button-primary community-full-width">CREATE POST</button></a>
-                                </div>
-                            </div>
-                            <div class="community-card">
-                                <div class="community-card-header">Choose a Channel</div>
-                                <div class="community-card-body">
-                                    ${showAllTags()}
-                                </div>
-                            </div>
-                            <div class="community-card">
-                                <div class="community-card-header">Community Rules</div>
-                                <div class="community-card-body">
-                                    <ol>
-                                        <li><strong>Be respectful</strong><br />Treat others how you'd like to be treated!</li>
-                                        <hr class="community-hr-minimal" />
-                                        <li><strong>No ads</strong><br />This is an ad-free zone</li>
-                                        <hr class="community-hr-minimal" />
-                                        <li><strong>Keepin it clean</strong><br />Use nice words only :)</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    ${navbar({shop, tags})}
                 </div>
             </main>
         </div>
