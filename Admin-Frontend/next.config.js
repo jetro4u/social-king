a8061f2ab565e6eb271ed24dfc3b281caa32b1c9
Elishakay <kramer1346@gmail.com>
2020-07-13 04:31:08 +0000

    nextjs build tests

--- Admin-Frontend/next.config.js
+++ Admin-Frontend/next.config.js
@@ -7,11 +7,11 @@ const apiKey =  JSON.stringify(process.env.SHOPIFY_API_KEY);
 module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: 'Shopify Tribe',
-    API_DEVELOPMENT: 'http://localhost:8000/api',
-    API_PRODUCTION: 'https://tribesquared.io/api',
+    API_DEVELOPMENT: 'https://amazontech.app/api',
+    API_PRODUCTION: 'https://amazontech.app/api',
     PRODUCTION: false,
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-    DOMAIN_PRODUCTION: 'https://tribesquared.io',
+    DOMAIN_PRODUCTION: 'https://amazontech.app',
     FB_APP_ID: '305241670454834',
     DISQUS_SHORTNAME: 'ampitup-io',
     GOOGLE_CLIENT_ID:'299145764723-o5k1tr51r339gua3cja4o02r0l2g4lkb.apps.googleusercontent.com'
