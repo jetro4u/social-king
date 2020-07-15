f077aa2f1642b8ff5412f0d47a0c539f619b3a11
Elisha Kramer <kramer1346@gmail.com>
2020-07-15 12:36:31 +0300

    saving image succesfully in nodejs filesystem from admin settings page

--- Admin-Backend/controllers/user.js
+++ Admin-Backend/controllers/user.js
@@ -2,6 +2,7 @@ const Shop = require('../models/shop');
 const User = require('../models/user');
 const Blog = require('../models/blog');
 const _ = require('lodash');
+const path = require('path');
 
 const fs = require('fs');
 const { errorHandler } = require('../helpers/dbErrorHandler');
@@ -36,6 +37,33 @@ exports.update = (req, res) => {
     console.log('shopName',shopName);
     let {newSettings} = req.body;
     console.log('req.body.files', req.body.files[0]);
+
+    var data = req.body.iconImg;
+
+    function decodeBase64Image(dataString) {
+      var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+
+      response.type = matches[1];
+      response.data = new Buffer(matches[2], 'base64');
+
+      return response;
+    }
+
+    var imageBuffer = decodeBase64Image(data);
+    
+    // let image = Buffer.from(req.body.iconImg, 'base64');
+    fs.writeFile(path.join(__dirname,'/l-file.jpg'), imageBuffer.data, function(err, result) {
+     if(err) {
+        console.log('error', err);
+    } else {
+        console.log('file saved successfully: ', result);
+    }
+   })
 
     Shop.findOne({ shopify_domain: shopName }).exec((err, oldShop) => {
         if (err) {
