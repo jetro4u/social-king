aa88217cbf38041bd199c0ed7b610ba8d00c5a3d
Elisha Kramer <kramer1346@gmail.com>
2020-07-16 15:16:13 +0300

    custom community name within the header, and pressing delete logs the id in the browser

--- Proxy/routes/liquid-templates/angular/components/managePosts.js
+++ Proxy/routes/liquid-templates/angular/components/managePosts.js
@@ -1,4 +1,15 @@
 const proxyRoute = process.env.PROXY_ROUTE;
+const moment = require('moment');
+
+function formatQuotes(str){
+   var reg = /"/g;
+   var newstr = `\\"`;
+   str = str.replace(reg,newstr);
+
+   var reg2 = /'/g;
+   newstr = "\\'"
+   return  str.replace(reg2,newstr);
+}
 
 module.exports.managePosts = ({user, posts}) => {
   console.log('user in managePosts function:', user); 
@@ -8,8 +19,11 @@ module.exports.managePosts = ({user, posts}) => {
       return `<div key=${i} class='pb-5'>
                   <a href='${proxyRoute}/blog/${blog.slug}'><h3>${blog.title}</h3></a>
                   <p class='mark'>
-                      Written by ${blog.postedBy.name} | Published on ${blog.updatedAt}
+                      Written by ${blog.postedBy.name} | Published on ${moment(blog.updatedAt).format('YYYY-MM-DD')}
                   </p>
+                  <button ng-click='deletePost(${formatQuotes(JSON.stringify(blog._id))})' class='btn btn-sm btn-danger'>
+                      Delete
+                  </button>
               </div>
       `;
   }).join(' ')
@@ -17,7 +31,7 @@ module.exports.managePosts = ({user, posts}) => {
   return `
       <div id='new-post' ng-controller='managePostsController'>
           <div id='error-message' class='text-center'>
-            <h3 >Manage Posts</h3>
+            <h3 >Manage Posts</h3><span ng-click='reloadPage()' class='reload'>&#x21bb;</span>
           </div>
           ${displayPosts(posts)}
       </div>`
@@ -28,7 +42,13 @@ module.exports.managePostsJS = (user) => {
     tribeApp.controller('managePostsController', function($scope, $http) {
       console.log('managePosts function ran');
       $scope.proxyRoute = '${proxyRoute}';
-
+      $scope.deletePost = function(post){
+        console.log('post to delete:',post);
+      }
+      $scope.reloadPage = function(post){
+        console.log('ran reloadPage func')
+        location.reload();
+      }
     });
   `
 }
