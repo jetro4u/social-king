ce97f5eb55972bda3280629d3df0635b444b6708
Elisha Kramer <kramer1346@gmail.com>
2020-07-19 13:13:29 +0300

    add comment view

---
+++ Proxy/models/comment.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const commentSchema = new mongoose.Schema(
+    {
+        postSlug: {
+            type: String,
+            unique: true,
+            index: true
+        },
+        body: {
+            type: Array,
+            required: true,
+            min: 200,
+            max: 2000000
+        },
+        html: {
+            type: String
+        },
+        excerpt: {
+            type: String,
+            max: 1000
+        },
+        hidden: { 
+            type: Boolean, 
+            default: true 
+        },
+        postedBy: {
+            type: ObjectId,
+            ref: 'User'
+        },
+        shopPostedAt: { 
+            type: ObjectId, 
+            ref: 'Shop'
+        },
+        shopifyDomain: { 
+            type: String
+        },
+        archivedByUser: {
+            type: Boolean
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Comment', commentSchema);
