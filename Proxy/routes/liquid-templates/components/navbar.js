5c056ffaaac8cc8f63ed3acbf9e9c35fe29f77b3
Elisha Kramer <kramer1346@gmail.com>
2020-07-21 12:27:22 +0300

    purecss navbar on userAdmin page

--- Proxy/routes/liquid-templates/components/navbar.js
+++ Proxy/routes/liquid-templates/components/navbar.js
@@ -1,6 +1,6 @@
 const proxyRoute = process.env.PROXY_ROUTE;
 
-exports.navbar = ({shop, tags, user}) => {
+exports.navbar = ({shop, tags, user, adminArea}) => {
 
     const showAllTags = () => {
         return tags.map((t, i) => `
@@ -71,7 +71,40 @@ exports.navbar = ({shop, tags, user}) => {
         </div>`
     }
 
-    return `
-        ${user ? showUserNav() : showBlogNav()}
-        `
+    const showUserAdminNav = () => {
+        return `<div class="pure-u-1-3">
+            <div class="community-pad-20">
+                <div class="community-card">
+                    <div class="community-card-header">${`Welcome ${user.name}`}</div>
+                    <img src='${user.cover_photo && user.cover_photo!='undefined' ? user.cover_photo : 'https://mysteryshopperblog.files.wordpress.com/2014/07/mystery-shopper-image.gif'}'
+                           alt="Profile Picture" class="img-responsive img-rounded">
+                    <div class="community-card-body">
+                    <b>General Bio: </b>${user.about}
+                    <hr class="community-hr" />
+                    <b>Store Favorites: </b>${user.storeFavorites}
+                    </div>
+                </div>
+                <div class="community-card">
+                    <div class="community-card-header">Community Rules</div>
+                    <div class="community-card-body">
+                        <ol>
+                            <li><strong>Be respectful</strong><br />Treat others how you'd like to be treated!</li>
+                            <hr class="community-hr-minimal" />
+                            <li><strong>No ads</strong><br />This is an ad-free zone</li>
+                            <hr class="community-hr-minimal" />
+                            <li><strong>Keepin it clean</strong><br />Use nice words only :)</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+
+    if(adminArea){
+        return showUserAdminNav();
+    } else if(user){
+        return showUserNav()
+    } else {
+        return showBlogNav()
+    }
 };
