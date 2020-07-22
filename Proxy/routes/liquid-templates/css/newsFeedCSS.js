cdd5a3a63d2915512805f73d2c3ee3c6faaaef65
Elisha Kramer <kramer1346@gmail.com>
2020-07-22 07:25:55 +0300

    mobile responsive blogsList view

--- Proxy/routes/liquid-templates/css/newsFeedCSS.js
+++ Proxy/routes/liquid-templates/css/newsFeedCSS.js
@@ -19,9 +19,11 @@ exports.newsFeedCSS = ({shop}) => {
             .community-header-title-box { display:flex }
             .community-header-title .community-h2 { display:inline-block;margin-left:20px;font-size:30px;font-weight:bold;color:#000;padding:10px 0; }
             .community-card { border:1px solid #ccc;border-radius:4px;margin-bottom:20px;background:#fff }
-            .community-card-header { background:darkblue;color:white;padding:10px;font-weight:bold; }
-            .community-card-body { padding:10px }
-            .community-pad-20 { padding:10px 20px 20px 20px }
+            .community-card-header { letter-spacing: .5px; background:darkblue;color:white;padding:10px;font-weight:bold; }
+            .community-card-body { letter-spacing: .4px; padding:10px }
+            @media only screen 
+            and (min-device-width: 600px)
+             { .community-pad-20 { padding:10px 20px 20px 20px } }
             .community-hr { margin:20px 0 }
             .community-hr-minimal { margin:8px 0 }
             .community-full-width { width: 100% }
