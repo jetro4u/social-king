4cb0eaa0ab8c68ce9b5fa2dc55e2cc6332a0af41
root <kramer1346@gmail.com>
2020-07-22 14:45:03 +0000

    fixes for deploymet on socialking.app

--- Admin-Frontend/next.config.js
+++ Admin-Frontend/next.config.js
@@ -6,12 +6,12 @@ const apiKey =  JSON.stringify(process.env.SHOPIFY_API_KEY);
 
 module.exports = withCSS({
   publicRuntimeConfig: {
-    APP_NAME: 'Shopify Tribe',
+    APP_NAME: 'Social King',
     API_DEVELOPMENT: 'http://localhost:8000/api',
-    API_PRODUCTION: 'https://amazonfashion.app/api',
+    API_PRODUCTION: 'https://socialking.app/api',
     PRODUCTION: process.env.NODE_ENV == 'production',
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-    DOMAIN_PRODUCTION: 'https://amazonfashion.app',
+    DOMAIN_PRODUCTION: 'https://socialking.app',
     FB_APP_ID: '305241670454834',
     DISQUS_SHORTNAME: 'ampitup-io',
     GOOGLE_CLIENT_ID:'299145764723-o5k1tr51r339gua3cja4o02r0l2g4lkb.apps.googleusercontent.com'
