0bd0553867a61d4567636c9638966c88aa4fa758
Elisha Kramer <kramer1346@gmail.com>
2020-07-28 12:46:49 +0300

    nicer design on tag buttons and the entire button is clickable

--- Proxy/routes/liquid-templates/css/newsFeedCSS.js
+++ Proxy/routes/liquid-templates/css/newsFeedCSS.js
@@ -42,9 +42,6 @@ exports.newsFeedCSS = ({shop}) => {
                   margin-bottom: 1em;
                   margin-right: 1em;
                   margin-left: 1em; background: rgb(66, 184, 221); color: white; border-radius: 4px; text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2); }
-            .community-button-secondary a:hover {
-              background-color: white;
-            }
             .community-checkbox { float:left;margin-right:30px }
         `
 };
