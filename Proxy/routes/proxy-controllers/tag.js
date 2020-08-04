6ffca38d5d550e340ce3b53f283f07096bf9a833
Elisha Kramer <kramer1346@gmail.com>
2020-08-04 11:44:02 +0300

    removed categories from inner join on mongoose query in tagcontroller

--- Proxy/routes/proxy-controllers/tag.js
+++ Proxy/routes/proxy-controllers/tag.js
@@ -31,6 +31,7 @@ exports.getTags = async (req, res, next) => {
             console.log('errer getting tags',err)
         }
         req.tags = data;
+        console.log('data in getTags db response:', data)
         next();
     });
 };
@@ -73,7 +74,6 @@ exports.read = (req, res) => {
                 }
                 // res.json(tag);
                 Blog.find({ tags: tag, hidden: false, archivedByUser: { $ne: true } })
-                    .populate('categories', '_id name slug')
                     .populate('tags', '_id name slug')
                     .populate('postedBy', '_id name username cover_photo')
                     .sort({ createdAt: -1 })
@@ -86,6 +86,7 @@ exports.read = (req, res) => {
                                 error: errorHandler(err)
                             });
                         }
+                        console.log('tagSlug object in view:', { shop, tags: req.tags, tag: tag, blogs: data, size: data.length });
                         res.send(tagSlug({ shop, tags: req.tags, tag: tag, blogs: data, size: data.length }));
                     });
             });
