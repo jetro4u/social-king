8900b630ed26def03423cc01985ed22b8885383c
Elisha Kramer <kramer1346@gmail.com>
2020-08-06 17:15:40 +0300

    on loading of the index page, the frontend checks for a charge_id in the url. If it's there, a post request is made to the server to update that the user has paid - this hasnt been finished yet, but the charge_id and shopifyDomin are getting logged on the server

--- Admin-Frontend/pages/index.js
+++ Admin-Frontend/pages/index.js
@@ -5,17 +5,22 @@ import ResourceListWithProducts from '../components/ResourceList';
 import Router from 'next/router'
 import { withRouter } from 'next/router'
 import { useRouter } from 'next/router'
+import { checkForCharge } from '../actions/auth';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-function Index() {
-	
-	const emptyState = !store.get('ids');
-  	console.log('store.get(ids): ',store.get('ids'))
-  	
-  	const router = useRouter()
+function Index(props) {
+	const router = useRouter()
     console.log('router.pathname',router.pathname)
 
+	if(router && router.query && router.query.charge_id) {
+		console.log('here is the charge_id', router.query.charge_id);	
+		checkForCharge(router.query.charge_id, props.app.shopOrigin)
+		console.log('props.app.shopOrigin', props.app.shopOrigin);
+	} 
+
+	const emptyState = !store.get('ids');
+  	
     return (
 	  <Page>
 	   <Layout>
