0ca44451fc6f182fa7577b0c2cb5d5bb366c4728
Elisha Kramer <kramer1346@gmail.com>
2020-08-06 16:42:47 +0300

    cleaned up react index.js component.

--- Admin-Frontend/pages/index.js
+++ Admin-Frontend/pages/index.js
@@ -3,14 +3,19 @@ import { ResourcePicker } from '@shopify/app-bridge-react';
 import store from 'store-js';
 import ResourceListWithProducts from '../components/ResourceList';
 import Router from 'next/router'
+import { withRouter } from 'next/router'
+import { useRouter } from 'next/router'
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-class Index extends React.Component {
-  state = { open: false };
-  render() {
-  	const emptyState = !store.get('ids');
+function Index() {
+	
+	const emptyState = !store.get('ids');
   	console.log('store.get(ids): ',store.get('ids'))
+  	
+  	const router = useRouter()
+    console.log('router.pathname',router.pathname)
+
     return (
 	  <Page>
 	   <Layout>
@@ -30,15 +35,6 @@ class Index extends React.Component {
 		    </Layout>
 	  </Page>
 	);
-  }
-
-  handleSelection = (resources) => {
-    const idsFromResources = resources.selection.map((product) => product.id);
-    this.setState({ open: false })
-    console.log(resources)
-    console.log(idsFromResources)
-    store.set('ids', idsFromResources);
-  };
 }
 
-export default Index;
+export default withRouter(Index);
