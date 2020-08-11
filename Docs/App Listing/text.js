d0546900083c8916aa68f8f8527cd589e321db8d
Elisha Kramer <kramer1346@gmail.com>
2020-08-11 04:42:39 +0300

    if in development test charge is true, else test charge is false

---
+++ Docs/App Listing/text.js
@@ -0,0 +1,23 @@
+
+
+Sometimes it takes more than product reviews to convince. An online community will provide unprecedented confidence to first-time shoppers.
+
+
+Social King gives your customers a reason to visit your store even when they're not shopping! Build community, and you get brand loyalty.
+
+
+
+FInal
+
+
+Enabling your shoppers to share their voices shows new shoppers that real people are buying your products and enjoying them.
+
+
+Show new shoppers that real people are buying your products and enjoying them
+
+
+
+
+
+
+In the world of SEO, content is king. Enable your customers to generate valuable content for you, without any heavy lifting!
