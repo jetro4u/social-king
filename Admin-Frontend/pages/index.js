ea2a6872dd82eafae427c09ca0541510b57fd9ce
Elisha Kramer <kramer1346@gmail.com>
2020-08-13 15:13:17 +0300

    The Admin app homepage now takes the user straight to the public community pages - immediately after install , and whenever the user signs in

--- Admin-Frontend/pages/index.js
+++ Admin-Frontend/pages/index.js
@@ -1,27 +1,33 @@
-import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { EmptyState, Layout, Page, Button } from '@shopify/polaris';
+import { useState, useEffect } from 'react';
 import Router from 'next/router'
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 function Index(props) {
 
+	const [shopDomain, setShopDomain] = useState('');
+    
 	const emptyState = true;
-  	
+  	console.log('props',props)
+
+  	useEffect(() => {
+        setShopDomain(props.app.shopOrigin)
+    }, []);
+
     return (
 	  <Page>
 	   <Layout>
 			    <EmptyState
 			        heading="Expower Shoppers to Share Their Voices"
-			        action={{
-			          content: 'Manage Posts',
-			          onAction:  () => Router.push('/manage/manage-posts'),
-			        }}
 			        image={img}
 			      >
-			   	   <p>Create a Social Network around your Brand, 
-			      		And Promote Related Products
-			      		Alongside User-Generated Content.</p>
-
+			   	   <p>Visit your {' '}
+                                <Button external={true} primary url={`https://${shopDomain}/community/connect`}>
+                                      Community Pages
+                                </Button>
+                                
+                             </p>
 		      </EmptyState>
 		    </Layout>
 	  </Page>
