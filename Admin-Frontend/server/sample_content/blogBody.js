48e2c6ab419a35135a252bea6b7ed44651176856
Elisha Kramer <kramer1346@gmail.com>
2020-08-19 11:38:46 +0300

    added links to external pages on first getting started post

--- Admin-Frontend/server/sample_content/blogBody.js
+++ Admin-Frontend/server/sample_content/blogBody.js
@@ -17,7 +17,19 @@ exports.blogBody = [
         {
           "type" : "paragraph",
           "data" : {
-            "text" : "Heyooo. We get it, Building a Social Community can be challenging, but we want to help you get there. That's why we've created 5 Steps to Kickstarting Your Own Social Network."
+            "text" : "Heyooo. We get it, Building a Social Community can be challenging, but we want to help you get there. That's why we've created <a target='_blank' href='https://social-king-app.myshopify.com/community/connect/blog/5-steps-to-building-a-shopper-community'>5 Steps to Kickstarting Your Own Social Network.</a>"
+          }
+        },
+        {
+          "type" : "paragraph",
+          "data" : {
+            "text" : "Please note that the app leverages your Shopify Store's Integrated Shopper Management System, so if you'd like to enable shoppers to leave Posts or Comments on your network, <a target='_blank' href='https://help.shopify.com/en/manual/checkout-settings/customer-accounts'>Customer Accounts should be Optional or Required.</a>"
+          }
+        },
+        {
+          "type" : "paragraph",
+          "data" : {
+            "text" : "If you need any assistance whatso-ever, please don't hesitate to reach out via email (help@socialking.app), or even better via our <a target='_blank' href='https://social-king-app.myshopify.com/community/connect/tags/faq'>demo shop's FAQ Section</a>. Thanks again for installing, and have an amazing day."
           }
         }
       ],
