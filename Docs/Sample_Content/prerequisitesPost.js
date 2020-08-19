722348eec124a706294c3d5478cec803fe3b9fca
Elisha Kramer <kramer1346@gmail.com>
2020-08-19 11:07:28 +0300

    cover_photo of new user on install is of Samantha Jones

---
+++ Docs/Sample_Content/prerequisitesPost.js
@@ -0,0 +1,65 @@
+db.blogs.update({ _id: ObjectId("5f3cd4c1af74db1ac8d46eff") }, {
+    $set: {
+        "updatedAt": ISODate("2020-08-19T10:29:06.181+03:00"),
+        "createdAt": ISODate("2020-08-19T10:29:05.806+03:00"),
+        "html": "<p> 1.&nbsp;Drive the conversation around specific topics by <a href=\"https://tribe-factor.myshopify.com/admin/apps/community-2/manage/manage-tags\">adding tags</a>. </p><p> 2.&nbsp;Customize the Look &amp; Feel of Your Network by <a href=\"https://tribe-factor.myshopify.com/admin/apps/community-2/settings\">adding Custom Images and Colors</a>. </p><p> 3.&nbsp;Verify that within your <a href=\"https://help.shopify.com/en/manual/checkout-settings/customer-accounts\">Store Settings</a>&nbsp;Customer Accounts are either optional or required. </p><p> 4. Use a Customer Account to create Some Posts - as a Customer, you can customize your Profile with a Profile Image, Store Favorites, and one-line Biography. </p><p> <br> </p>",
+        "postedBy": ObjectId("5f3cd196765cde2ba410bb62"),
+        "mdesc": "1. Drive the conversation around specific topics by adding tags.",
+        "excerpt": "1.&nbsp;Drive the conversation around specific topics by <a href=\"https://tribe-factor.myshopify.com/admin/apps/community-2/manage/manage-tags\">adding tags</a>.",
+        "mtitle": "Prerequisites | Social King",
+        "slug": "prerequisites",
+        "coverMedia": "",
+        "title": "Prerequisites",
+        "shopifyDomain": "tribe-factor.myshopify.com",
+        "userNotified": false,
+        "archivedByUser": false,
+        "tags": [
+            ObjectId("5f2baa44bc34d72f20f28bbd")
+        ],
+        "categories": [],
+        "relatedProducts": [],
+        "selectedProducts": [],
+        "comments": [],
+        "hidden": true,
+        "body": [
+            {
+                "version": "2.18.0",
+                "blocks": [
+                    {
+                        "data": {
+                            "text": "1.&nbsp;Drive the conversation around specific topics by <a href=\"https://tribe-factor.myshopify.com/admin/apps/community-2/manage/manage-tags\">adding tags</a>."
+                        },
+                        "type": "paragraph"
+                    },
+                    {
+                        "data": {
+                            "text": "2.&nbsp;Customize the Look &amp; Feel of Your Network by <a href=\"https://tribe-factor.myshopify.com/admin/apps/community-2/settings\">adding Custom Images and Colors</a>."
+                        },
+                        "type": "paragraph"
+                    },
+                    {
+                        "data": {
+                            "text": "3.&nbsp;Verify that within your <a href=\"https://help.shopify.com/en/manual/checkout-settings/customer-accounts\">Store Settings</a>&nbsp;Customer Accounts are either optional or required."
+                        },
+                        "type": "paragraph"
+                    },
+                    {
+                        "data": {
+                            "text": "4. Use a Customer Account to create Some Posts - as a Customer, you can customize your Profile with a Profile Image, Store Favorites, and one-line Biography."
+                        },
+                        "type": "paragraph"
+                    },
+                    {
+                        "data": {
+                            "text": "<br>"
+                        },
+                        "type": "paragraph"
+                    }
+                ],
+                "time": 1597822144936
+            }
+        ],
+        "__v": NumberInt(0),
+        "shopPostedAt": ObjectId("5f3cce22b8358416b8b0563f")
+    }
+})
