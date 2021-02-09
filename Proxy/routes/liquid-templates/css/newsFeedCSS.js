9254b7f5e22e5c6bc8425dc0c9f51b92f5b5de2e
elishakay <kramer1346@gmail.com>
2021-02-09 14:10:58 +0200

    handling cases of no CSSCode Attribute on the given shop - ie - backwards compatability for shops that didnt have their CSS Code autogenerated within the db

--- Proxy/routes/liquid-templates/css/newsFeedCSS.js
+++ Proxy/routes/liquid-templates/css/newsFeedCSS.js
@@ -1,10 +1,16 @@
+const { defaultCSS } = require('./defaultCSS');
+
 exports.newsFeedCSS = ({shop}) => {
     const backupHeaderImg = "https://www.dorothylane.com/wp-content/uploads/2017/09/cheese.jpg";
 
-    return `${shop.CSSCode}
-             .community-background { background-color: ${shop && shop.backgroundColor ? shop.backgroundColor : '#543434' }; }
-             .community-header { background-image: url(${shop && shop.headerImageURL ? shop.headerImageURL : backupHeaderImg}); }
-             .community-card-header { background:${shop && shop.primaryColor ? shop.primaryColor : '#26b598'}; }
-             .community-instant-post { border:1px solid ${shop && shop.backgroundColor ? shop.backgroundColor : '#edeff1'}; }
-        `;
+    if(!shop){
+    	return defaultCSS()
+    } else {
+	    return `${shop.CSSCode}
+	             .community-background { background-color: ${shop && shop.backgroundColor ? shop.backgroundColor : '#543434' }; }
+	             .community-header { background-image: url(${shop && shop.headerImageURL ? shop.headerImageURL : backupHeaderImg}); }
+	             .community-card-header { background:${shop && shop.primaryColor ? shop.primaryColor : '#26b598'}; }
+	             .community-instant-post { border:1px solid ${shop && shop.backgroundColor ? shop.backgroundColor : '#edeff1'}; }
+	        `;
+	}
 };
