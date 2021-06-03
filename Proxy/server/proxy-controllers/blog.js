866344a85167d8f68587668dacc0afdfd20d602b
elishakay <kramer1346@gmail.com>
2021-06-03 13:56:02 +0300

    created sendEmail function

--- Proxy/server/proxy-controllers/blog.js
+++ Proxy/server/proxy-controllers/blog.js
@@ -95,23 +95,7 @@ exports.create = (req, res) => {
            let appSlug = process.env.NODE_ENV == 'development' ? 'community-2' : 'social-king';
            
            if(shop && shop._doc && shop._doc.postModeration){
-               const emailData = {
-                  to: shop && shop._doc && shop._doc.extraShopifyData && shop._doc.extraShopifyData[0] && shop._doc.extraShopifyData[0].email ? shop._doc.extraShopifyData[0].email : 'kramer1346@gmail.com',
-                  from: 'help@socialking.app',
-                  subject: `Review A New Community Post!`,
-                  text: `Hey ${storeAdminName}, \n Looks like a new post has been submitted via your Community Network`,
-                  html: `
-                      <h4>Hey ${storeAdminName},</h4>
-                      <p>A New Customer Post has been Submitted and is <a href='https://${blog.shopifyDomain}/admin/apps/${appSlug}/manage/manage-posts'>pending review here</a></p>
-                      <hr />
-                  `
-               };
-               
-               console.log('emailData in Sendgrid Email Notification Function', emailData);
-        
-               sgMail.send(emailData).then(sent => {
-                    console.log('email alert sent to ', req.query.shop)
-               })
+               sendMail()
            }           
            
            Blog.findByIdAndUpdate(result._id, { $set: { shopPostedAt: [shop._id] } }, { new: true }).exec(
